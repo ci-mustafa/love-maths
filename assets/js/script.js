71464d73ef452e181ed972b51ad621f790afa1c4
ci-mustafa <akbarimustafa64@gmail.com>
2024-04-18 19:50:42 +0000

    feat: generate two random numbers inside runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // wait the DOM to finish loading before running the game
 // get the button elements and add event listener to them
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName('button');
     for (let button of buttons) {
-        button.addEventListener("click", function(){
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked submit")
             } else {
@@ -14,9 +14,17 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 })
 
+/**
+ * The main game 'loop', called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
-
+    // generating two random numbers
+    let firstRandomNumber = Math.floor(Math.random() * 25) + 1;
+    let secondRandomNumber = Math.floor(Math.random() * 25) + 1;
 }
+
+runGame();
 
 function checkAnswer() {
 
@@ -47,6 +55,6 @@ function displayMultiplicationQuestion() {
 }
 
 function displayDivisionQuestion() {
-    
+
 }
 
